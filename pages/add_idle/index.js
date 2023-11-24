3b25e53f266befa8e413c60d0f42554d6fec745f
newpc <2258532120@qq.com>
2023-11-24 13:08:06 +0800

    feat：上架功能，实现到等后台

--- pages/add_idle/index.js
+++ pages/add_idle/index.js
@@ -1,84 +1,93 @@
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    text:"",
-    pics:[]
+    text: "",
+    pics: [],
+    InfoFlag: 0,
+    booksCount:1
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
-  },
+  onLoad: function (options) {},
   addPic() {
-    let {pics} = this.data
+    let { pics } = this.data;
     wx.chooseMedia({
-      camera: 'back',
+      camera: "back",
       count: 9,
       maxDuration: 3,
-      mediaType: ['image'],
-      sizeType: ['compressed'],
-      sourceType: ['album','camera'],
+      mediaType: ["image"],
+      sizeType: ["compressed"],
+      sourceType: ["album", "camera"],
       success: (result) => {
         console.log(result);
-        let {tempFiles} = result
+        let { tempFiles } = result;
         if (pics.length + tempFiles.length <= 9) {
-          pics.push(...tempFiles)
+          pics.push(...tempFiles);
           this.setData({
-            pics
-          })
-        }else{
+            pics,
+          });
+        } else {
           wx.showToast({
-            title: '上传的太多了',
-            icon:'none',
+            title: "上传的太多了",
+            icon: "none",
             duration: 1500,
             mask: true,
           });
-            
         }
-        
       },
       fail: (res) => {
         wx.showToast({
-          title: '上传失败',
-          icon: 'none',
+          title: "上传失败",
+          icon: "none",
           duration: 1500,
           mask: true,
         });
         console.log(res);
       },
-    })
+    });
   },
-  // getUserInfomation() {
-  //   wx.getUserInfo({
-  //     withCredentials: 'false',
-  //     lang: 'zh_CN',
-  //     timeout:10000,
-  //     success: (result) => {
-        
-  //     },
-  //     fail: () => {},
-  //     complete: () => {}
-  //   });
-      
-  // },
-  // handleUserInfo(e) {
-  //   console.log(e);
-  //   const {userInfo} = e.detail;
-  //   wx.setStorageSync("userInfo", userInfo);
-  //   // this.setData({userInfo});
-  //   wx.navigateBack({
-  //     delta: 1
-  //   });
-  // }
+  onTapImage(e) {
+    let { pics } = this.data;
+    console.log(pics);
+    console.log(e);
+    let picsUrl = [];
+    pics.forEach((element) => {
+      picsUrl.push(element.tempFilePath);
+    });
+    wx.previewImage({
+      current: e.currentTarget.dataset.src,
+      urls: picsUrl,
+      success: (result) => {
+        console.log(result);
+      },
+      fail: (res) => {
+        console.log(res);
+      },
+      complete: () => {},
+    });
+  },
+  cancel(e) {
+    let { pics } = this.data;
+    console.log(e.currentTarget.dataset.index);
+    const newpcis = pics.filter((v, index) => 
+      index !== e.currentTarget.dataset.index
+    );
+    console.log(newpcis);
+    this.setData({ pics:newpcis });
+  },
   getUserInfo1() {
     console.log("1");
-    let {globaldata} = getApp()
-    this.setData({globaldata})
-    
+    let { globaldata } = getApp();
+    this.setData({ globaldata });
+    this.setData({ InfoFlag: 1 });
+  },
+  addkuang() {
+    let {booksCount} = this.data
+    booksCount++
+    this.setData({booksCount})
   }
-})
+});
--- pages/add_service/index.js
+++ pages/add_service/index.js
@@ -1,23 +1,93 @@
-// pages/add_service/index.js
-Component({
+Page({
   /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
+   * 页面的初始数据
    */
   data: {
-
+    text: "",
+    pics: [],
+    InfoFlag: 0,
+    booksCount:1
   },
 
   /**
-   * 组件的方法列表
+   * 生命周期函数--监听页面加载
    */
-  methods: {
-
+  onLoad: function (options) {},
+  addPic() {
+    let { pics } = this.data;
+    wx.chooseMedia({
+      camera: "back",
+      count: 9,
+      maxDuration: 3,
+      mediaType: ["image"],
+      sizeType: ["compressed"],
+      sourceType: ["album", "camera"],
+      success: (result) => {
+        console.log(result);
+        let { tempFiles } = result;
+        if (pics.length + tempFiles.length <= 9) {
+          pics.push(...tempFiles);
+          this.setData({
+            pics,
+          });
+        } else {
+          wx.showToast({
+            title: "上传的太多了",
+            icon: "none",
+            duration: 1500,
+            mask: true,
+          });
+        }
+      },
+      fail: (res) => {
+        wx.showToast({
+          title: "上传失败",
+          icon: "none",
+          duration: 1500,
+          mask: true,
+        });
+        console.log(res);
+      },
+    });
+  },
+  onTapImage(e) {
+    let { pics } = this.data;
+    console.log(pics);
+    console.log(e);
+    let picsUrl = [];
+    pics.forEach((element) => {
+      picsUrl.push(element.tempFilePath);
+    });
+    wx.previewImage({
+      current: e.currentTarget.dataset.src,
+      urls: picsUrl,
+      success: (result) => {
+        console.log(result);
+      },
+      fail: (res) => {
+        console.log(res);
+      },
+      complete: () => {},
+    });
+  },
+  cancel(e) {
+    let { pics } = this.data;
+    console.log(e.currentTarget.dataset.index);
+    const newpcis = pics.filter((v, index) => 
+      index !== e.currentTarget.dataset.index
+    );
+    console.log(newpcis);
+    this.setData({ pics:newpcis });
+  },
+  getUserInfo1() {
+    console.log("1");
+    let { globaldata } = getApp();
+    this.setData({ globaldata });
+    this.setData({ InfoFlag: 1 });
+  },
+  addkuang() {
+    let {booksCount} = this.data
+    booksCount++
+    this.setData({booksCount})
   }
-})
+});
